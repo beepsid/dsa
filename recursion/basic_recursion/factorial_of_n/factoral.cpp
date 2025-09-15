3beace9f1fa2b32d4da2030dad3f381022957f28
ΛYUSH <siddhantsinghchauhan1@gmail.com>
2025-09-15 21:40:07 +0530

    factorial in recursion

---
+++ recursion/basic_recursion/factorial_of_n/factoral.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fact(int n){
+    if(n==0){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number you want factorial of: ";
+    cin>>n;
+    printf("%d",fact(n));
+}
